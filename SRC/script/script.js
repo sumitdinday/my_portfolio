3ed1d7b9dfb4d59fc9d1119c42b4a941aae530ae
Sumit dinday <pc@Sumits-MacBook-Air.local>
2025-06-16 19:55:50 +0530

    Initial website upload

---
+++ SRC/script/script.js
@@ -0,0 +1,37 @@
+let menu = document.querySelector('#menu-icon-js');
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let navtc = document.querySelector('#nav-tc-js');
+
+menu.onclick = () => {
+	menuicon.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
+	navtc.classList.toggle("nav-touch-close-open");
+}
+
+navtc.onclick = () => {
+	menuicon.classList.toggle('bx-x');
+	navbar.classList.remove('open');
+	navtc.classList.remove("nav-touch-close-open");
+	navtc.classList.remove("nav-tc-z");
+	navtc.classList.remove("nav-LR-TC");
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+	var currentScrollPos = window.pageYOffset;
+
+	document.getElementById("header").classList.add('scrolled');
+	if (currentScrollPos === 0) {
+		document.getElementById("header").classList.remove('scrolled');
+	}
+	if (navtc.classList.contains('nav-touch-close-open')) {
+		return;
+	}
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("header").style.top = "0";
+	} else {
+		document.getElementById("header").style.top = "-100px";
+	}
+	prevScrollpos = currentScrollPos;
+}
